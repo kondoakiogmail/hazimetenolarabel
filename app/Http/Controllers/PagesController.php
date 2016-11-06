82378a8bdf81b5ac681cefe80b97e25d5f4e2b4f
kondo.akio <kondo.akio@headwaters.co.jp>
2016-11-06 16:13:49 +0900

    (4) ビューへ値を渡す

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,6 +7,15 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
     public function about() {
+        // 配列に値をセット
+        $data = [];
+        $data["first_name"] = "Luke";
+        $data["last_name"] = "Skywalker";
+
+        // view関数の第２引数に配列を渡す
+        return view('pages.about', $data);
+
+        /* 以前のコード
         return view('pages.about');
-    }
+        */    }
 }
