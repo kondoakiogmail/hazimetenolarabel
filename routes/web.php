eeda921798419aa7ca5d2547308c1dba908b75ee
kondo.akio <kondo.akio@headwaters.co.jp>
2016-11-09 19:23:25 +0900

    (7) マイグレーションでDBのバージョン管理

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
-|
-*/
-//
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-//
-
-// app/Http/routes.php
-
 Route::get('/', 'WelcomeController@index');
 Route::get('contact', 'WelcomeController@contact');
 Route::get('about', 'PagesController@about');
